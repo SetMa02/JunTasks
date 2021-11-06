44356ad1852889a2c248eb9eda8d535eb327ce7f
Максим Майнагашев <maxuser99@mail.ru>
2021-11-06 15:44:02 +0700

    ImagersTask

---
+++ z1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace z1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int photos = 52;
+            int countAtoneRow = 3;
+
+            int rowsCount = photos / countAtoneRow;
+            int overflowCount = photos % countAtoneRow;
+
+            Console.WriteLine("Заполнится рядов - " + rowsCount);
+            Console.WriteLine("Картинок сверх меры - " + overflowCount);
+
+
+            Console.ReadLine();
+        }
+    }
+}
