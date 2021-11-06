e5575e83d56930b3bf8ec9dfbaef1bc6137d2ddf
Максим Майнагашев <maxuser99@mail.ru>
2021-11-06 16:55:57 +0700

    UserAsking

---
+++ UserAsking/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace UserAsking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Как вас зовут ");
+            string name = Console.ReadLine();
+            Console.Write("Сколько вам лет? ");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Какой ваш знак зодиака ");
+            string zodiacSign = Console.ReadLine();
+            Console.Write("Ваше место работы ");
+            string placeOfWork = Console.ReadLine();
+            Console.WriteLine($"Вас зовут {name}, вам {age} год, вы {zodiacSign} и работаете на {placeOfWork}.");
+        }
+    }
+}
