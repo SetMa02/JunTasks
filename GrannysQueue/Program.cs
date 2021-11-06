dcb54ddd4b82ad30d5babacdbd8569d3cb7f4f50
Максим Майнагашев <maxuser99@mail.ru>
2021-11-06 16:51:47 +0700

    GrannysQueue

---
+++ GrannysQueue/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace GrannysQueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfGrannys;
+            int timeOfReceipt = 10;
+
+            Console.WriteLine("Введите кол-во старушек: ");
+            numberOfGrannys = Convert.ToInt32(Console.ReadLine());
+
+            int totalMinutes = numberOfGrannys * timeOfReceipt;
+            int minutesBeforeAdmission = totalMinutes % 60;
+
+            Console.WriteLine("Вы должны отстоять в очереди "+ (totalMinutes / 60) +" часа и "+ minutesBeforeAdmission +" минут." );
+        }
+    }
+}
