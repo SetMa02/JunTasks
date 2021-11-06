5db32b6f474a21539f4209c0936cdac5f635e14d
Максим Майнагашев <maxuser99@mail.ru>
2021-11-06 17:48:35 +0700

    Chars

---
+++ Chars/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Chars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Использую цикл FOR потомучто есть четкое обозначения конца цикла. While удобен бля бесконечного цикла. foreach для массивов*/
+
+            int maxValue = 98;
+            int step = 7;
+            
+            for (int i = 7; i <= maxValue; i = i + 7)
+            {
+                Console.Write(i + " ");
+            }
+        }
+    }
+}
