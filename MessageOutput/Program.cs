7190ac752f36dd03113495b0d1701796f3732c80
Максим Майнагашев <maxuser99@mail.ru>
2021-11-06 16:57:45 +0700

    MessageOutput

---
+++ MessageOutput/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MessageOutput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите сообщение: ");
+            string message = Console.ReadLine();
+            Console.WriteLine("Введите количество повторений: ");
+            int count = Int32.Parse(Console.ReadLine());
+
+            for (int i = 0; i < count; i++)
+            {
+                Console.WriteLine(message);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
