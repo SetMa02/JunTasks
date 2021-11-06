6729f9dc51d80c7aa31ac6eaa918cfa5eb6c53d0
Максим Майнагашев <maxuser99@mail.ru>
2021-11-06 15:47:25 +0700

    GoldForCrustals Added

---
+++ GoldForCrystals/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace GoldForCrystals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int goldsCount = 0;
+            int cristalsCount = 0;
+            int priceCristal = 3;
+
+            Console.Write("Введите ваше количество золота: ");
+            goldsCount = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Цена за 1 кристалл - " + priceCristal);
+            Console.WriteLine("Сколько кристаллов вы хотите купить?");
+            cristalsCount = Convert.ToInt32(Console.ReadLine());
+
+            goldsCount -= (cristalsCount * priceCristal);
+
+            Console.WriteLine();
+            Console.WriteLine("Вы приобрели кристаллов в кол-ве: " + cristalsCount);
+            Console.WriteLine("Остаток золота: " + goldsCount);
+        }
+    }
+}
