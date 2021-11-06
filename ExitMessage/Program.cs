5033f6404d3d28863230cb02804507da1cbc54cd
Максим Майнагашев <maxuser99@mail.ru>
2021-11-06 17:03:51 +0700

    ExitMessage

---
+++ ExitMessage/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ExitMessage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isExit = false;
+            while (isExit == false)
+            {
+                Console.WriteLine("Введите сообщение:");
+                if (Console.ReadLine() == "exit")
+                {
+                    isExit = true;
+                }
+            }
+        }
+    }
+}
