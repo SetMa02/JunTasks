1876eef768b18334124428238bdb1ccad7aa294f
Максим Майнагашев <maxuser99@mail.ru>
2021-11-07 14:39:11 +0700

    StringPutput fixed

---
+++ StringOutput/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace StringOutput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите имя:");
+            string word = Console.ReadLine();
+            Console.WriteLine("Введите символ:");
+            string sumbol = Console.ReadLine();
+
+            int wordLengh = word.Length;
+
+            for (int i = 1; i <= 3; i++)
+            {
+                if (i == 1)
+                {
+                    for (int b = 1; b <= wordLengh + 2; b++)
+                    {
+                        Console.Write(sumbol);
+                    }
+                }
+                else if (i == 2)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine(sumbol + word + sumbol);
+                }
+                else if (i == 3)
+                {
+                    for (int b = 1; b <= wordLengh + 2; b++)
+                    {
+                        Console.Write(sumbol);
+                    }
+                }                        
+             
+            }
+        }
+    }
+}
