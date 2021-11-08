884f6ab3e46edf99a47c7e4be0c732114b017537
Максим Майнагашев <maxuser99@mail.ru>
2021-11-09 00:00:30 +0700

    Symboloutput Fixed

--- StringOutput/Program.cs
+++ StringOutput/Program.cs
@@ -9,32 +9,21 @@ namespace StringOutput
             Console.WriteLine("Введите имя:");
             string word = Console.ReadLine();
             Console.WriteLine("Введите символ:");
-            string sumbol = Console.ReadLine();
+            string symbol = Console.ReadLine();
 
             int wordLengh = word.Length;
-
-            for (int i = 1; i <= 3; i++)
+            
+            for (int b = 1; b <= wordLengh + 2; b++)
             {
-                if (i == 1)
-                {
-                    for (int b = 1; b <= wordLengh + 2; b++)
-                    {
-                        Console.Write(sumbol);
-                    }
-                }
-                else if (i == 2)
-                {
-                    Console.WriteLine();
-                    Console.WriteLine(sumbol + word + sumbol);
-                }
-                else if (i == 3)
-                {
-                    for (int b = 1; b <= wordLengh + 2; b++)
-                    {
-                        Console.Write(sumbol);
-                    }
-                }                        
-             
+                Console.Write(symbol);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine(symbol + word + symbol);
+            
+            for (int b = 1; b <= wordLengh + 2; b++)
+            {
+                Console.Write(symbol);
             }
         }
     }
