32e6be5aefac3e9f1095bc58bcbc135be0524c85
Максим Майнагашев <maxuser99@mail.ru>
2021-11-13 22:50:14 +0700

    WordDescription1

---
+++ WordDescription/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WordDescription
+{
+    class Program
+    {
+        static void Main()
+        {
+            Dictionary<string, string> items = new Dictionary<string, string>();
+            
+            items.Add("стул", "Стульчкик посидеть.");
+            items.Add("стол", "стол кухонный, хороший.");
+            items.Add("холодильник", "Самый лучший предмет.");
+            items.Add("телевизор", "Кго уже давно не смотрят.");
+            
+            string userInput;
+
+            bool isExit = false;
+ 
+            while (isExit == false)
+            {
+                Console.Write("Введите название предмета: ");
+                userInput = Console.ReadLine();
+ 
+                if (items.ContainsKey(userInput.ToLower()))
+                    Console.WriteLine(items[userInput]);
+                else
+                    Console.WriteLine("Нераспознал");
+                if (userInput == "exit")
+                    isExit = true;
+
+            }
+        }
+    }
+}
