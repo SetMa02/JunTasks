e77eedaf479ca3ed5243080385f0942bf411a981
Максим Майнагашев <maxuser99@mail.ru>
2021-11-13 22:32:46 +0700

    ConvertToDigit

---
+++ ConvertToDigit/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Globalization;
+using System.Runtime.InteropServices;
+
+namespace ConvertToDigit
+{
+    class Program
+    {
+   
+        static void Main(string[] args)
+        {
+            int convertedDigit;
+            
+            ConvertToInt(out convertedDigit);
+            
+            Console.WriteLine(convertedDigit);
+        }
+
+        public static void ConvertToInt(out int Digit)
+        {
+            bool isExit = false;
+            int inputDigit = 0;
+            
+            while (isExit == false)
+            {
+                Console.Clear();
+                Console.WriteLine("Введите число: ");
+
+                isExit = Int32.TryParse(Console.ReadLine(), out inputDigit);
+                
+            }
+
+            Digit = inputDigit;
+        }
+    }
+}
