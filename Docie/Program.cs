97b49cd27fba47037ee1e29de299ed22ddb3072e
Максим Майнагашев <maxuser99@mail.ru>
2021-11-13 15:13:02 +0700

    Docie Fixed

--- Docie/Program.cs
+++ Docie/Program.cs
@@ -7,8 +7,8 @@ namespace Docie
     {
         static void Main(string[] args)
         {
-            string[] fioTable = new string[] { };
-            string[] workTable = new string[] { };
+            string[] employeesTable = new string[] { };
+            string[] worksTable = new string[] { };
             bool isExit = false;
 
             while (isExit == false)
@@ -16,19 +16,23 @@ namespace Docie
                 Console.WriteLine("1 - Добавить сотрудника \n" +
                                   "2 - Удалить сотрудника \n" +
                                   "3 - Просмотр сотрудников \n" +
-                                  "4 - Выход");
+                                  "4 - Поиск сотрудника \n" +
+                                  "5 - Выход");
                 switch (Console.ReadLine())
                 {
                     case "1":
-                        AddEmployee(ref fioTable, ref workTable);
+                        AddEmployee(ref employeesTable, ref worksTable);
                         break;
                     case "2":
-                        DeleteEmployee(ref fioTable, ref workTable);
+                        DeleteEmployee(ref employeesTable, ref worksTable);
                         break;
                     case "3":
-                        ShowEmploee(ref fioTable, ref workTable);
+                        ShowEmploee(employeesTable, worksTable);
                         break;
                     case "4":
+                        FindEmployee(employeesTable, worksTable);
+                        break;
+                    case "5":
                         isExit = true;
                         break;
                     default:
@@ -38,48 +42,80 @@ namespace Docie
             }
         }
 
-        public static void AddEmployee(ref string[] fio, ref string[] work)
+        public static void FindEmployee(string[] employee, string[] works)
+        {
+            if (employee.Length == 0)
+            {
+                Console.WriteLine("Искать нечего!");
+                return;
+            }
+
+            Console.Clear();
+            Console.WriteLine("Введите фамилию сотрудника: ");
+            string searchEmployee = Console.ReadLine();
+            int indexOfEmployee = Array.IndexOf(employee, searchEmployee);
+            Console.WriteLine(indexOfEmployee + 1 + ". " + employee[indexOfEmployee] + " - " + works[indexOfEmployee]);
+        }
+
+        public static void AddEmployee(ref string[] employee, ref string[] works)
         {
             Console.Clear();
             Console.Write("Введите ФИО сотрудника - ");
-            Array.Resize(ref fio, fio.Length + 1);
-            fio[fio.Length - 1] = Console.ReadLine();
+            Array.Resize(ref employee, employee.Length + 1);
+            employee[employee.Length - 1] = Console.ReadLine();
 
             Console.WriteLine();
             Console.Write("Введите должность - ");
-            Array.Resize(ref work, work.Length + 1);
-            work[work.Length - 1] = Console.ReadLine();
+            Array.Resize(ref works, works.Length + 1);
+            works[works.Length - 1] = Console.ReadLine();
         }
 
-        public static void DeleteEmployee(ref string[] fio, ref string[] work)
+        public static void DeleteEmployee(ref string[] employee, ref string[] works)
         {
-            Console.Clear();
-            Console.WriteLine("Какого сотрудника удалить?");
-            for (int i = 0; i < fio.Length; i++)
+            if (employee.Length == 0)
             {
-                Console.WriteLine(i + 1 + ". " + fio[i] + " - " + work[i]);
+                Console.WriteLine("Удалять нечего!");
+                return;
             }
 
-            int indexToRemove = Convert.ToInt32(Console.ReadLine());
-            var fioList = fio.ToList();
-            var workList = work.ToList();
-            
-            workList.RemoveAt(indexToRemove - 1);
-            fioList.RemoveAt(indexToRemove - 1);
-            
-            fio = fioList.ToArray();
-            work = workList.ToArray();
+            Console.Clear();
+            Console.WriteLine("Какого сотрудника удалить?");
+            for (int i = 0; i < employee.Length; i++)
+            {
+                Console.WriteLine(i + 1 + ". " + employee[i] + " - " + works[i]);
+            }
+
+            int indexToDelete = Convert.ToInt32(Console.ReadLine());
+
+            RemoveAt(ref employee, indexToDelete - 1);
+            RemoveAt(ref works, indexToDelete - 1);
         }
 
-        public static void ShowEmploee(ref string[] fio, ref string[] work)
+        public static void ShowEmploee(string[] employee, string[] works)
         {
-            Console.Clear();
-            for (int i = 0; i < fio.Length; i++)
+            if (employee.Length == 0)
             {
-                Console.WriteLine(i + 1 + ". " + fio[i] + " - " + work[i]);
+                Console.WriteLine("Показывать нечего!");
+                return;
+            }
+
+            Console.Clear();
+            for (int i = 0; i < employee.Length; i++)
+            {
+                Console.WriteLine(i + 1 + ". " + employee[i] + " - " + works[i]);
             }
 
             Console.WriteLine();
+        }
+
+        public static void RemoveAt<T>(ref T[] arrAy, int index)
+        {
+            for (int i = index; i < arrAy.Length - 1; i++)
+            {
+                arrAy[i] = arrAy[i + 1];
+            }
+
+            Array.Resize(ref arrAy, arrAy.Length - 1);
         }
     }
 }
