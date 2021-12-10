ae08b51d79a98047cd1960d67f03b45b11bea059
Максим Майнагашев <maxuser99@mail.ru>
2021-12-10 10:54:41 +0700

    ListSumm added

---
+++ ListSumm/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ListSumm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> listOfDigits = new List<int>();
+            bool isExit = false;
+            int sum;
+            while (isExit == false)
+            {
+                string input = Console.ReadLine();
+                switch (input)
+                {
+                    case "sum":
+                        sum = listOfDigits.ToArray().Sum();
+                        Console.WriteLine("Сумма: " + sum);
+                        break;
+                    case "exit":
+                        isExit = true;
+                        break;
+                    default:
+                        listOfDigits.Add(Convert.ToInt32(input));
+                        break;
+                }
+            }
+        }
+    }
+}
