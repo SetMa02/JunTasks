642c73e1fc5d148e6f71bb271384cf7141373eb7
Максим Майнагашев <maxuser99@mail.ru>
2021-12-11 17:09:36 +0700

    PlayerClass addde

---
+++ PlayerClass/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace PlayerClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Player player = new Player();
+            
+            Player.ShowStatus(player);
+        }
+    }
+
+    class Player
+    {
+        private string name;
+        private string playerClass;
+
+        public Player()
+        {
+            name = "SetMa02";
+            playerClass = "маг";
+        }
+
+        public static void ShowStatus(Player player)
+        {
+            Console.WriteLine(player.name + " " + player.playerClass);
+        }
+    }
+}
