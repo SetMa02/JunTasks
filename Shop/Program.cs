0f0f77d07e41991eabf475001086e568459ad8d9
Максим Майнагашев <maxuser99@mail.ru>
2022-01-02 16:54:34 +0700

    Shop Fixed

--- Shop/Program.cs
+++ Shop/Program.cs
@@ -10,10 +10,12 @@ namespace Shop
             bool isExit = false;
 
             Seller seller = new Seller();
-            Player player = new Player();
+
+            Player player = new Player(300);
 
             while (isExit == false)
             {
+                Console.WriteLine("Денег - " + player.Money);
                 Console.WriteLine();
                 Console.WriteLine("1 - Просмотр товара \n" +
                                   "2 - Купить товар \n" +
@@ -22,19 +24,12 @@ namespace Shop
                 switch (Console.ReadLine())
                 {
                     case "1":
-                        Console.Clear();
                         seller.ShowItems();
                         break;
                     case "2":
-                        int buyIndex;
-                        Console.Clear();
-                        Console.WriteLine("Какой товар хотите купить?");
-                        seller.ShowItems();
-                        buyIndex = Convert.ToInt32(Console.ReadLine());
-                        player.AddItem(seller.SellItem(buyIndex - 1));
+                        BuyItem();
                         break;
                     case "3":
-                        Console.Clear();
                         player.ShowItems();
                         break;
                     case "4":
@@ -45,73 +40,102 @@ namespace Shop
                         break;
                 }
             }
+
+            void BuyItem()
+            {
+                int buyIndex;
+                Console.Clear();
+                Console.WriteLine("Какой товар хотите купить?");
+                seller.ShowItems();
+                if (!Int32.TryParse(Console.ReadLine(), out buyIndex))
+                {
+                    Console.WriteLine("Неверный индекс!");
+                    BuyItem();
+                }
+                else
+                {
+                    player.AddItem(seller.SellItem(buyIndex - 1));
+                }
+            }
         }
     }
 
-    class Player
+    class Profile
     {
-        private List<Item> _backPack;
+        protected List<Item> Items;
 
-        public Player()
+        public Profile(List<Item> items)
         {
-            _backPack = new List<Item>() { };
+            Items = items;
+        }
+
+        public void ShowItems()
+        {
+            Console.WriteLine();
+            for (int i = 0; i < Items.Count; i++)
+            {
+                Console.WriteLine(i + 1 + ". " + Items[i].Name + " - " + Items[i].Cost);
+            }
+        }
+    }
+
+    class Player : Profile
+    {
+        private int _money;
+        public int Money => _money;
+
+        public Player(int money) : base(new List<Item>())
+        {
+            _money = money;
         }
 
         public void AddItem(Item item)
         {
-            _backPack.Add(item);
-        }
-
-        public void ShowItems()
-        {
-            foreach (var item in _backPack)
+            if (_money - item.Cost >= 0)
             {
-                Console.WriteLine(item.Name);
+                _money -= item.Cost;
+                Items.Add(item);
+            }
+            else
+            {
+                Console.WriteLine("Недостаточно денег!");
             }
         }
     }
 
-    class Seller
+    class Seller : Profile
     {
-        private List<Item> _items;
-
-        public Seller()
+        public Seller() : base(new List<Item>()
         {
-            _items = new List<Item>()
-            {
-                new Item("Меч"),
-                new Item("Посох"),
-                new Item("Лук"),
-                new Item("Щит"),
-                new Item("Кинжал")
-            };
+            new Item("Меч", 100),
+            new Item("Посох", 150),
+            new Item("Лук", 75),
+            new Item("Щит", 125),
+            new Item("Кинжал", 50)
+        })
+        {
         }
 
         public Item SellItem(int id)
         {
-            Item SellItem = _items[id];
-            _items.RemoveAt(id);
-            return SellItem;
-        }
-
-        public void ShowItems()
-        {
-            for (int i = 0; i < _items.Count; i++)
-            {
-                Console.WriteLine(i + 1 + ". " + _items[i].Name);
-            }
+            Item sellItem = Items[id];
+            Items.RemoveAt(id);
+            return sellItem;
         }
     }
 
     class Item
     {
         private string _name;
+        private int _cost;
 
         public string Name => _name;
+        public int Cost => _cost;
 
-        public Item(string name)
+        public Item(string name, int cost)
         {
             _name = name;
+            _cost = cost;
         }
     }
 }
