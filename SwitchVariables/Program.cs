54c27f19196bea12bc9053cb479022ff20eda4c4
Максим Майнагашев <maxuser99@mail.ru>
2022-04-29 16:27:22 +0700

    Switch variables added

---
+++ SwitchVariables/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace SwitchVariables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName;
+            string secondName;
+            string buffer;
+            
+            Console.WriteLine("Введите имя");
+            secondName = Console.ReadLine();
+            Console.WriteLine("Введите фамилию");
+            firstName = Console.ReadLine();
+
+            buffer = secondName;
+            secondName = firstName;
+            firstName = buffer;
+            
+            Console.WriteLine($"Имя - {firstName}, Фамилия - {secondName}");
+        }
+    }
+}
