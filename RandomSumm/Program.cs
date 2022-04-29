5e34915b6af80466e487ff6f6de59f23b4810529
Максим Майнагашев <maxuser99@mail.ru>
2022-04-29 22:35:22 +0700

    RandomSumm Fixed

--- RandomSumm/Program.cs
+++ RandomSumm/Program.cs
@@ -9,12 +9,14 @@ namespace RandomSumm
             Random random = new Random();
             int maxNumber = 100;
             int minNumber = 0;
+            int firstDeleteNumber = 3;
+            int secondDeleteNumber = 5;
             int randomNumber = random.Next(minNumber, maxNumber);
             int summ = 0;
             
             for (int i = 0; i <= randomNumber; i++)
             {
-                if (i % 3 == 0 || i % 5 == 0)
+                if (i % firstDeleteNumber == 0 || i % secondDeleteNumber == 0)
                 {
                     summ += i;
                 }
