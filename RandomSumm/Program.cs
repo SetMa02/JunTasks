2dccf7e3c28c91170e3f49496fc8e888ee9712ef
Максим Майнагашев <maxuser99@mail.ru>
2022-04-29 16:32:59 +0700

    RandomSumm added

---
+++ RandomSumm/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace RandomSumm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int randomNumber = random.Next(0, 100);
+            int summ = 0;
+            
+            for (int i = 0; i <= randomNumber; i++)
+            {
+                if (i % 3 == 0 || i % 5 == 0)
+                {
+                    summ += i;
+                }
+            }
+            
+            Console.WriteLine(summ);
+        }
+    }
+}
