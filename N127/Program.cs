58cfc268f9d211a8ca95c4bf03f98ce1a71fc3c4
Максим Майнагашев <maxuser99@mail.ru>
2022-04-30 21:20:02 +0700

    N127 Fixed

--- N127/Program.cs
+++ N127/Program.cs
@@ -9,16 +9,16 @@ namespace N127
             Random random = new Random();
             int firstNumber = 1;
             int lastNumber = 27;
-            int n = random.Next(firstNumber, lastNumber);
+            int number = random.Next(firstNumber, lastNumber);
             int count = 0;
             int minNumber = 100;
             int maxNumber = 1000;
             
             for(int i = minNumber; i < maxNumber; ++i)
             {
-                string str = i.ToString();
+                string newNumber = i.ToString();
         
-                if(int.Parse(str[0].ToString()) + int.Parse(str[1].ToString()) + int.Parse(str[2].ToString()) == n)
+                if(int.Parse(newNumber[0].ToString()) + int.Parse(newNumber[1].ToString()) + int.Parse(newNumber[2].ToString()) == number)
                 {
                     count++;
                 }
