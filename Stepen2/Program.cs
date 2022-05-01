aa1d6ce6ff2cf95a440bbb937401003c8fc1044c
Максим Майнагашев <maxuser99@mail.ru>
2022-05-01 14:28:13 +0700

    Stepen 2 added

---
+++ Stepen2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Stepen2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int result = 1;
+            int stepen = 0;
+            int minNumber = 4;
+            int maxNumber = 100;
+            int number = random.Next(minNumber, maxNumber);
+
+            while (number > result)
+            {
+                result *= 2;
+                stepen++;
+            }
+            
+            Console.Write($"{number}, {stepen}, {result}");
+        }
+    }
+}
