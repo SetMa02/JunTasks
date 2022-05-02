435f212d1bc397953a797413f0661af6887290d7
Максим Майнагашев <maxuser99@mail.ru>
2022-05-02 17:32:13 +0700

    String split added

---
+++ StringSplit/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace StringSplit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text;
+            string[] massive;
+            
+            Console.WriteLine("Введите текст:");
+
+            text = Console.ReadLine();
+
+            massive = text.Split(" ");
+
+            foreach (var word in massive)
+            {
+                Console.WriteLine(word);
+            }
+        }
+    }
+}
